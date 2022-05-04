ae8840df962b275267239b80447a5877dc079058
andalons <andreacor001@gmail.com>
2022-05-04 16:07:48 +0200

    add useEffect to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+
+import callToApi from '../services/callToApi';
+
 function App() {
+  useEffect(() => {
+    callToApi().then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return <div className="App">Hola mundo</div>;
 }
 
